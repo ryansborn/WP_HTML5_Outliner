d0aa5b557c8a396d6722ff66853e1db82240ede5
Ryan Born <41238593+ryansborn@users.noreply.github.com>
2018-12-08 10:22:43 -0500

    Shorten Toolbar node title. Update class name.
    Toolbar node title is now just "Outline", rather than "HTML5 Outliner." The class HTML5_Outline was renamed to "Document_Outline". The instantiation of that class has been revised accordingly. Also, source formatting was improved.

--- classes/WP_HTML5_Outliner.php
+++ classes/WP_HTML5_Outliner.php
@@ -86,10 +86,10 @@ class WP_HTML5_Outliner {
 	 */
 	public static function add_to_toolbar( $wp_admin_bar ) {
 
-		// Adds an 'HTML5 Outliner' text node to the Toolbar. 
+		// Adds an 'Outline' text node to the Toolbar. 
 		$wp_admin_bar->add_node( [
 			'id'    => __NAMESPACE__ . '-outliner-toolbar-node',
-			'title' => __( 'HTML5 Outliner', 'wph50'),
+			'title' => __( 'Outline', 'wph50'),
 			'meta'  => [ 'class' => __NAMESPACE__ . '-outliner-toolbar-node' ],
 		] );
 
@@ -122,57 +122,58 @@ class WP_HTML5_Outliner {
 		 * Tells user if there is no outline. Appears in place of the 
 		 * Heading-level outline by default.
 		 */
-		$notice = sprintf( '<div class="%2$s-notice">%1$s</div>',
-							__( 'No outline was created.', 'wph5o' ),
-							__NAMESPACE__ 
-				  );
+		$notice = sprintf(
+			'<div class="%2$s-notice">%1$s</div>',
+			__( 'No outline was created.', 'wph5o' ),
+			__NAMESPACE__ 
+		);
 
 		if ( self::$page ) {
 
-			$outline = ( new HTML5_Outline( self::$page ) )->render_outline();
+			$outline = ( new Document_Outline( self::$page ) )->render_outline();
 
 		}
 
 		if ( empty( $outline ) ) {
 
-			return sprintf( '<div class="%2$s-wrapper">%1$s</div>',
-							 $notice,
-							 __NAMESPACE__ 
-				   );
+			return sprintf(
+				'<div class="%2$s-wrapper">%1$s</div>',
+				$notice,
+				__NAMESPACE__ 
+			);
 
 		}
 		
-		return sprintf( '<div class="%9$s-wrapper">
-						   <div id="%9$s-popper" title="%1$s" aria-hidden="true">
-						   </div><!-- #%9$s-popper -->
-						   <div id="%9$s-outline-toggle" aria-hidden="true">
-						     <a href="#%9$s-structural" id="%9$s-show-s" class="%9$s-active" 
-							    title="%2$s">%3$s</a>
-							 &nbsp; | &nbsp;
-							 <a href="#%9$s-h-level" id="%9$s-show-hl" 
-							    title="%2$s">%4$s</a>
-						   </div><!-- #%9$s-outline-toggle -->
-						   <div id="%9$s-outline">
-							 <div id="%9$s-structural">
-							   <p class="%9$s-sr-only %9$s-outline-title">%5$s</p>
-							     %6$s
-							 </div><!-- #%9$s-structural -->
-							 <div id="%9$s-h-level" class="%9$s-sr-only">
-							   <p class="%9$s-sr-only %9$s-outline-title">%7$s</p>
-							     %8$s
-						     </div><!-- #%9$s-h-level -->
-						   </div><!-- #%9$s-outline -->
-						 </div><!-- .%9$s-wrapper -->',
-						 esc_attr__( 'Open in new window', 'wph5o' ),
-						 esc_attr__( 'Select Outline Type', 'wph5o' ),
-						 __( 'Structural (HTML5)', 'wph5o' ),
-						 __( 'Heading-level', 'wph5o' ),
-						 __( 'Structural (HTML5) Outline', 'wph5o' ),
-						 $outline,
-						 __( 'Heading-level Outline', 'wph5o' ),
-						 $notice,
-						 __NAMESPACE__
-			   );
+		return sprintf( 
+			'<div class="%9$s-wrapper">
+				<div id="%9$s-popper" title="%1$s" aria-hidden="true">
+				</div><!-- #%9$s-popper -->
+				<div id="%9$s-outline-toggle" aria-hidden="true">
+					<a href="#%9$s-structural" id="%9$s-show-s" class="%9$s-active" title="%2$s">%3$s</a>
+					&nbsp; | &nbsp;
+					<a href="#%9$s-h-level" id="%9$s-show-hl" title="%2$s">%4$s</a>
+				</div><!-- #%9$s-outline-toggle -->
+				<div id="%9$s-outline">
+					<div id="%9$s-structural">
+						<p class="%9$s-sr-only %9$s-outline-title">%5$s</p>
+						%6$s
+					</div><!-- #%9$s-structural -->
+					<div id="%9$s-h-level" class="%9$s-sr-only">
+						<p class="%9$s-sr-only %9$s-outline-title">%7$s</p>
+						%8$s
+					</div><!-- #%9$s-h-level -->
+				</div><!-- #%9$s-outline -->
+			</div><!-- .%9$s-wrapper -->',
+			esc_attr__( 'Open in new window', 'wph5o' ),
+			esc_attr__( 'Select Outline Type', 'wph5o' ),
+			__( 'Structural (HTML5)', 'wph5o' ),
+			__( 'Heading-level', 'wph5o' ),
+			__( 'Structural (HTML5) Outline', 'wph5o' ),
+			$outline,
+			__( 'Heading-level Outline', 'wph5o' ),
+			$notice,
+			__NAMESPACE__
+		);
 
 	}
 
